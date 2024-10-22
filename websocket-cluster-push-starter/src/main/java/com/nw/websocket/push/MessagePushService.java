71fcc48f0ed21d343c10c52fed4fc8164b069dd1
ningwei <1458630585@qq.com>
2024-10-22 15:40:00 +0800

    MessagePushService优化

--- websocket-cluster-push-starter/src/main/java/com/nw/websocket/push/MessagePushService.java
+++ websocket-cluster-push-starter/src/main/java/com/nw/websocket/push/MessagePushService.java
@@ -7,8 +7,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 
-import java.util.concurrent.ExecutionException;
-
 /**
  * 消息推送服务类，负责通过Kafka将消息发送到指定的主题
  */
@@ -24,11 +22,9 @@ public class MessagePushService {
      * 发送消息到Kafka主题
      *
      * @param message 待发送的消息对象
-     * @throws ExecutionException   如果异步发送操作抛出异常
-     * @throws InterruptedException 如果当前线程在等待发送操作完成时被中断
      */
-    public void send(Message message) throws ExecutionException, InterruptedException {
+    public void send(Message message) {
         // 将消息转换为JSON字符串，然后通过Kafka模板发送到指定的主题，使用用户ID作为键，确保消息的路由
-        kafkaTemplate.send(websocketProperties.getTopic(), message.getUserId(), JSON.toJSONString(message)).get();
+        kafkaTemplate.send(websocketProperties.getTopic(), message.getUserId(), JSON.toJSONString(message)).join();
     }
 }
